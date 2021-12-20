9946b164440669dcb73d9f486d7fa8410b7bd346
drissbejgana <93830096+drissbejgana@users.noreply.github.com>
2021-12-21 00:01:03 +0100

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,11 +2,11 @@ import './header.css';
 import React, {useContext } from 'react';
 import {ThemContext} from '../../App'
 
-export default function Header({mode,func}){
+export default function Header({func}){
     const theme=useContext(ThemContext)
     return(
         <div style={{backgroundColor:theme.body,boxShadow:theme.boxshadow}} className="container">
-             <h2  style={{color:theme.text}} >Portfolio.</h2>
+             <h2  style={{color:theme.text}} >Portfo<span>lio.</span></h2>
              <ul style={{backgroundColor:theme.body}}  className="nav-bar">
                  <li><a style={{color:theme.text}}  href="#home" >Home</a></li>
                  <li><a style={{color:theme.text}}  href="#skills" >Skills</a></li>
@@ -15,15 +15,15 @@ export default function Header({mode,func}){
                  <li><a style={{color:theme.text}}  href="#contact">Contact</a></li>
              </ul>
                 <div className="img">
-                    <img onClick={()=>func()}  src={mode} alt="" />
+                    <img onClick={()=>func()}  src={theme.logomode} alt="" />
                 </div>
                 <div onClick={
                          ()=>{
-                            document.querySelector(".nav-bar").classList.toggle('trans')
-                            document.getElementById("row1").classList.toggle('rows1')
-                            document.getElementById("row2").classList.toggle('rows2')
-                            document.getElementById("row3").classList.toggle('rows3')
-                         }
+                                document.querySelector(".nav-bar").classList.toggle('trans')
+                                document.getElementById("row1").classList.toggle('rows1')
+                                document.getElementById("row2").classList.toggle('rows2')
+                                document.getElementById("row3").classList.toggle('rows3')
+                             }
                 } className="burger">
                     <div style={{backgroundColor:theme.text}} id="row1" className="scrollb" ></div>
                     <div style={{backgroundColor:theme.text}} id="row2" className="scrollb"></div>
