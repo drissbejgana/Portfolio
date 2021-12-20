8d852e746adad9170ea1a0b2464cff88616658c3
drissbejgana <93830096+drissbejgana@users.noreply.github.com>
2021-12-21 00:00:00 +0100

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,24 @@
+import bat from  './img/bat.png' 
+import butterfly from  './img/butterfly.png' 
+import dark from  './img/moon.png' 
+import light from  './img/sunshine.png' 
 export const lightTheme = {
     body: "whitesmoke",
     text: "#060514",
     boxshadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",
-    imgfly:"./img/butterfly.png"
+    imgfly:butterfly,
+    logomode:dark
   };
   
  export const darkTheme = {
     body: "#060514",
     text: "#FFFFFF",
     boxshadow: "rgba(212, 198, 198, 0.24) 0px 3px 8px",
-    imgfly:"./img/bat.png"
+    imgfly:bat,
+    logomode:light
+
   };
+  
+  
   
   
